94e90934a846ff860d9e01ab9fb928a4038cadd7
Alexander Merkulov <sasha@merqlove.ru>
2017-05-23 09:34:48 +0300

    Fix pgdb repo path

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,11 @@
+default['postgresql']['pgdg']['repo_rpm_url']['9.3']['centos'] ||= {} 
+default['postgresql']['pgdg']['repo_rpm_url']['9.3']['centos'] = node['postgresql']['pgdg']['repo_rpm_url']['9.3']['centos'].merge({
+  "6" => {
+    "i386" => "http://yum.postgresql.org/9.3/redhat/rhel-6-i386/pgdg-centos93-9.3-3.noarch.rpm",
+    "x86_64" => "http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-3.noarch.rpm"
+  }
+})
+
 default['postgresql']['pgdg']['repo_rpm_url']['9.4']['centos'] = {
   "7" => {
     "x86_64" => "http://yum.postgresql.org/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-1.noarch.rpm"
@@ -18,4 +26,3 @@ when 'debian', 'ubuntu'
 when 'rhel', 'fedora', 'suse'
   default['postgresql']['enable_pgdg_yum'] = true
 end
-
