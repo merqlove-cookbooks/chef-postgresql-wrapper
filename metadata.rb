dab3101428a6ca4ff3c0a0dc52d5ca336bec697c
Alexander Merkulov <sasha@merqlove.ru>
2019-05-22 09:45:22 +0300

    Upgrade version 0.0.5

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'sasha@merqlove.ru'
 license 'all_rights'
 description 'Installs/Configures postgresql'
 long_description 'Installs/Configures postgresql'
-version '0.0.4'
+version '0.0.5'
 
 depends 'postgresql', '3.4.20'
